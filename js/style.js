846d47d14e611ea52b347415462ed3fab8b2297b
Yutaka Obuchi <sonzaikannousuiningen+fms@gmail.com>
2014-10-13 21:33:19 +0900

    3: 完成
    ﾖｯｼｬ

--- js/style.js
+++ js/style.js
@@ -1,4 +1,3 @@
-console.log(marked(document.getElementById("article").innerHTML));
 document.getElementById("article").innerHTML=marked(document.getElementById("article").innerHTML);
 
 function resize()
